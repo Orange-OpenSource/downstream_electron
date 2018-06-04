566fca733e2f1127ac9f2193300ac59cf1a0a240
Bertrand Berthelot <bertrand.berthelot@orange.com>
2018-06-04 14:41:10 +0200

    get-offline-link: enable retrieving offline link before download is finished

--- api/be-methods/downloads/get-offline-link.js
+++ api/be-methods/downloads/get-offline-link.js
@@ -7,14 +7,10 @@ module.exports = function (api, onSuccess, onFailure, target, manifestId) {
     if (err) {
       onFailure(translation.getError(translation.e.downloads._GENERAL), err);
     } else {
-      if (info.status === STATUSES.FINISHED) {
-        onSuccess({
-          offlineLink: api.getOfflinePath(manifestId) + info.manifest.name,
-          persistent: info.persistent
-        });
-      } else {
-        onFailure(translation.getError(translation.e.downloads.UNFINISHED, manifestId), err);
-      }
+      onSuccess({
+        offlineLink: api.getOfflinePath(manifestId) + info.manifest.name,
+        persistent: info.persistent
+      });
     }
   });
 };
