ea7a171ad6d91dd7bc13748e697853a03bcb3c45
Bertrand Berthelot <bertrand.berthelot@orange.com>
2018-07-18 11:43:19 +0200

    Revert "Update downloads-controller.js"
    This reverts commit c6870d2b83afa75cd707d1ee6fbf6f9932689103.

--- api/controllers/downloads-controller.js
+++ api/controllers/downloads-controller.js
@@ -671,6 +671,8 @@ DownloadsController.prototype.startQueue = function (nextManifestPositionInArray
       this.storage.status.setItem(manifestId, "status", STATUSES.WAITING);
     }
     return;
+  } else {
+    this.storage.status.setItem(manifestId, "status", STATUSES.STARTED);
   }
 
   if (!manifestId) {
