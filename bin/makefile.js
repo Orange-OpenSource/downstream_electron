181e96a658170846815b74ab1019bd518ef0697a
Jeremie COLLET <jeremie.collet@orange.com>
2019-03-06 09:39:49 +0100

    Merge with master

---
+++ bin/makefile.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+
+function usage() {
+    console.log('Usage: ' + __filename + ' prepack | postpack | clean');
+    process.exit(-1);
+}
+
+if (process.argv.length <= 2) {
+    usage();
+}
+
+const command = process.argv[2];
+const fs = require('fs');
+const packageFile = 'package.json';
+
+if (command === 'prepack') {
+    fs.readFile(packageFile, 'utf8', (err, data) => {
+        if (err) {
+            return console.log(err);
+        }
+        var result = data.replace(/api\/index\.js/g, 'index.js');
+        result = result.replace(/"files": \["api"\],\n  /g, '');
+
+        fs.writeFile(packageFile, result, 'utf8', function (err) {
+            if (err) return console.log(err);
+        });
+    });
+} else if (command === 'postpack') {
+    fs.readFile(packageFile, 'utf8', (err, data) => {
+        if (err) {
+            return console.log(err);
+        }
+        var result = data.replace(/index.js",/g, 'api/index.js",\n  "files": ["api"],');
+
+        fs.writeFile(packageFile, result, 'utf8', function (err) {
+            if (err) return console.log(err);
+        });
+    });
+} else if (command === 'clean') {
+    fs.unlink('index.js', (err) => {
+        console.log('index.js is deleted');
+    });
+    fs.unlink('downstream-electron-be.js', (err) => {
+        console.log('downstream-electron-be.js is deleted');
+    });
+    fs.unlink('downstream-electron-fe.js', (err) => {
+        console.log('downstream-electron-fe.js is deleted');
+    });
+    fs.unlink('startServer.js', (err) => {
+        console.log('startServer.js is deleted');
+    });
+} else {
+    usage();
+}
